3939ddde46c202f548c9cd78930adc39035f2718
Benjamin <benj@berkeley.edu>
2012-04-04 20:10:54 -0700

    added remote repository page

--- WpfApplication1/WpfApplication1/obj/x86/Debug/WpfApplication1_Content.g.i.cs
+++ WpfApplication1/WpfApplication1/obj/x86/Debug/WpfApplication1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/pushbutton.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/menuremote.png")]
 
 
